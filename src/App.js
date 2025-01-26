2d0e282443a0c3b5163abf7cb5e92afd688fcf68
adithya <adithya@guvi.in>
2025-01-26 10:00:14 +0530

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App my-5">
+      Esse reprehenderit laboris commodo nostrud voluptate reprehenderit ea laborum ullamco pariatur quis do magna. Fugiat non aute labore ea sunt commodo anim. Nostrud veniam eu sint et incididunt non irure labore deserunt duis consectetur. Et enim irure officia officia ad.
+    Aute ex ipsum duis commodo. Dolore laborum culpa adipisicing enim nostrud ullamco labore dolore labore. Veniam sint ad aliquip deserunt. Ex voluptate officia magna mollit aliqua esse.Dolor nulla proident sit nisi nisi nisi proident. Mollit laborum enim et excepteur nostrud ea sint deserunt do quis labore nisi. Sint dolore veniam non qui cillum cillum. Dolore ad cillum velit incididunt esse culpa sunt anim.Ex fugiat irure proident nulla in. Cupidatat ad ad do non. Laboris id qui est occaecat fugiat nulla labore consectetur exercitation do officia id. Deserunt irure qui incididunt quis quis dolore et sunt ex et proident aliquip adipisicing.
+    Do aliquip do velit sunt irure dolor ea. Deserunt eu laboris deserunt cupidatat id aute commodo. Tempor incididunt esse amet nulla eiusmod excepteur Lorem dolor. Exercitation ut nostrud enim officia esse amet cupidatat voluptate consequat. Ea dolor eu officia voluptate tempor qui id sunt id fugiat consectetur. Magna aliqua aliqua reprehenderit sit id voluptate enim ullamco labore quis.
+    Lorem ullamco ipsum officia proident enim. Voluptate voluptate ullamco laboris velit consectetur reprehenderit in. Tempor ex tempor est sit in sunt eiusmod qui ex laborum laborum eiusmod labore nostrud. Duis esse consequat consectetur officia nostrud ullamco irure non. Cillum magna ea id irure.
     </div>
   );
 }
